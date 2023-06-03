1d14dd4847665f7571cfd8c592c4b5361723c567
Vaibhavi Singh <contactvaibhavisingh@gmail.com>
2023-06-03 23:32:15 +0530

    Fixes #84
    Moved hardcoded configs to environment variables
    
    Used python library decouple and module config to refer the environment variables. Keeping the env variables locally in a .env file.

--- data.py
+++ data.py
@@ -1,13 +1,9 @@
 from twilio.rest import Client
+from decouple import config
 
-account_sid = 'AC4813fd548f065ee195b1aa2feb96c58f' # Found on Twilio Console Dashboard
-auth_token = 'cda8141ecc84ab6ae0489aacab4d3586' # Found on Twilio Console Dashboard
-myPhone = '+919013908350' # Phone number you used to verify your Twilio account
-TwilioNumber = '+12692206694' # Phone number given to you by Twilio
-
-client = Client(account_sid, auth_token)
+client = Client(config('ACCOUNT_SID'), config('AUTH_TOKEN'))
 
 client.messages.create(
-        to=myPhone,
-        from_=TwilioNumber,
+        to=config('MYPHONE'),
+        from_=config('TWILIO_NUMBER'),
         body='I sent a text message from twilio! ' + u'\U0001f680')
